c92be704ad2e4de53ec3f8819f5aa12b377410f9
Raymond Chavez <chavezr@mit.edu>
2018-07-04 16:43:42 -0400

    Create searchable list of locations

--- src/pages/pay/location/location.ts
+++ src/pages/pay/location/location.ts
@@ -6,24 +6,26 @@ import {
   ViewController,
 } from 'ionic-angular';
 
-/**
- * Generated class for the LocationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-location',
   templateUrl: 'location.html',
 })
 export class LocationPage {
+  locations: {
+    name: string;
+    city: string;
+    streetAddress: string;
+    distance: number;
+  }[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController
-  ) {}
+  ) {
+    this.getLocations();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LocationPage');
@@ -38,7 +40,51 @@ export class LocationPage {
     );
   }
 
+  filterItems(ev) {
+    this.getLocations();
+    const { value } = ev.target;
+    if (value && value.trim() !== '') {
+      this.locations = this.locations.filter(
+        location =>
+          location.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+      );
+    }
+  }
+
+  getLocations() {
+    this.locations = [
+      {
+        name: 'The Smoke Shop BBQ',
+        city: 'Cambridge, MA',
+        streetAddress: '25 Hampshire St.',
+        distance: 0.01,
+      },
+      {
+        name: "Mamaleh's Delicatessen",
+        city: 'Cambridge, MA',
+        streetAddress: '15 Hampshire St.',
+        distance: 0.01,
+      },
+      {
+        name: 'Naco Taco',
+        city: 'Cambridge, MA',
+        streetAddress: '297 Massachusetts Ave.',
+        distance: 0.6,
+      },
+      {
+        name: 'The Smoke Shop BBQ - Seaport',
+        city: 'Boston, MA',
+        streetAddress: '343 Congress St.',
+        distance: 3.0,
+      },
+    ];
+  }
+
   next() {
     this.navCtrl.push('TabLookupPage');
+  }
+
+  selectLocation(location) {
+    this.navCtrl.push('TabLookupPage', location);
   }
 }
