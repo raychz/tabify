0729f8185d4f1530c290ed50c5eab6d61f6e662f
Raymond Chavez <chavezr@mit.edu>
2018-07-17 20:30:15 -0400

    WIP on newsfeed updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,27 +1,36 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
+export interface Story {
+  locationName: string;
+  members: string[];
+  timestamp;
+  likes: number;
+  comments: number;
+}
+
 @IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
   selectedSegment = 'user';
   global = [];
   community = [];
   user = [];
-  
-  constructor(public navCtrl: NavController) {
 
-  }
+  constructor(public navCtrl: NavController) {}
 
   segmentChanged(event) {
     console.log(event);
   }
 
   payNewTab() {
-    this.navCtrl.push('PayPage', {}, { animate: true, animation: 'md-transition', direction: 'forward' });
+    this.navCtrl.push(
+      'PayPage',
+      {},
+      { animate: true, animation: 'md-transition', direction: 'forward' }
+    );
   }
-
 }
