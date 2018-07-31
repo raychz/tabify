a91b8b91efa6ed815eac13005408158e4baf0f07
Raymond Chavez <chavezr@mit.edu>
2018-07-30 23:50:21 -0400

    Make tip adjustable

--- src/pages/pay/tax-tip/tax-tip.ts
+++ src/pages/pay/tax-tip/tax-tip.ts
@@ -7,12 +7,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tax-tip.html',
 })
 export class TaxTipPage {
-
+  tip = 20;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TaxTipPage');
+  }
+
+  adjustTip(shouldIncreaseTip) {
+    if(this.tip > 0) {
+      this.tip = shouldIncreaseTip ? this.tip + 1 : this.tip - 1;
+    } else if (shouldIncreaseTip) {
+      this.tip += 1;
+    }
   }
 
 }
