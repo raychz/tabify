b561c59e9257bf3b9795f2e983ff5631b7031e10
Raymond Chavez <chavezr@mit.edu>
2018-08-02 22:17:04 -0400

    Add config options for socket connection

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,16 @@
-export const firebaseConfig = {
-  fire: {
-    apiKey: "AIzaSyCyOe9TB0DTkDtX8B_3ddutEfN0nOy-WdU",
-    authDomain: "tabify-40746.firebaseapp.com",
-    databaseURL: "https://tabify-40746.firebaseio.com",
-    projectId: "tabify-40746",
-    storageBucket: "tabify-40746.appspot.com",
-    messagingSenderId: "1044249255712"
-  }
+export default {
+  firebaseConfig: {
+    fire: {
+      apiKey: 'AIzaSyCyOe9TB0DTkDtX8B_3ddutEfN0nOy-WdU',
+      authDomain: 'tabify-40746.firebaseapp.com',
+      databaseURL: 'https://tabify-40746.firebaseio.com',
+      projectId: 'tabify-40746',
+      storageBucket: 'tabify-40746.appspot.com',
+      messagingSenderId: '1044249255712',
+    },
+  },
+  ioConfig: {
+    url: 'http://localhost:3000',
+    options: {},
+  },
 };
