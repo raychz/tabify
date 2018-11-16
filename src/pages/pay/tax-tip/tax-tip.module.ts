d2825328d8861b7275fe7f8a7529c812cb338fe0
Raymond Chavez <chavezr@mit.edu>
2018-11-16 12:39:20 -0500

    Add basic 5-star rating component to selected tab items

--- src/pages/pay/tax-tip/tax-tip.module.ts
+++ src/pages/pay/tax-tip/tax-tip.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TaxTipPage } from './tax-tip';
+import { StarRatingModule } from 'ionic3-star-rating';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { TaxTipPage } from './tax-tip';
   ],
   imports: [
     IonicPageModule.forChild(TaxTipPage),
+    StarRatingModule
   ],
 })
 export class TaxTipPageModule {}
