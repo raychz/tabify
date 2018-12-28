3e45f0679149b194121839b6d516383c9dc2e1e3
Davi DeBarros <davi_debarros@my.uri.edu>
2018-12-27 22:35:39 -0500

    changed back to use static string, was causing some issues

--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,31 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ng-socket-io';
 import { Platform } from 'ionic-angular';
-import { AuthService } from "../auth/auth.service";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class ExtendedSocket extends Socket {
-  private readonly HOST = platform.is('android')
-      ? 'http://192.168.1.31'
-      : 'http://localhost';
-  private readonly PORT = 3000;
-  private readonly NSPS = 'ticket-events';
-
-  constructor(public platform: Platform, public authService: AuthService) {
-    const url = `${this.HOST}:${this.PORT}/${this.NSPS}`;
-    const options = {
-      query: {
-        'uid': this.authService.getUid(),
-      }
-    };
-    super({ url, options });
-
-
-    console.log('IS PLATFORM RUNNING ANRDOIRD '), platform.is('android');
+  constructor(public platform: Platform, authService: AuthService) {
+    super({
+      url: platform.is('android')
+        ? 'http://192.168.1.31:3000/ticket-events'
+        : 'http://localhost:3000/ticket-events',
+      options: {
+        query: {
+          uid: authService.getUid(),
+        }
+      },
+    });
+    console.log('IS PLATFORM RUNNING ANRDOIRD ', platform.is('android'));
     console.log('HERE');
-  }
-
-  connect() {
-    super.connect();
   }
 }
