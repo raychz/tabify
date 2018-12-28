ad682a5b9f4365cb1f30b6ddd5252a905ecde8dd
Davi DeBarros <davi_debarros@my.uri.edu>
2018-12-27 22:06:07 -0500

    changed how we create the socket

--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ng-socket-io';
 import { Platform } from 'ionic-angular';
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class ExtendedSocket extends Socket {
-  constructor(public platform: Platform) {
-    super({
-      url: platform.is('android')
-        ? 'http://192.168.1.31:3000'
-        : 'http://localhost:3000',
-      options: {},
-    });
+  private readonly HOST = platform.is('android')
+      ? 'http://192.168.1.31'
+      : 'http://localhost';
+  private readonly PORT = 3000;
+  private readonly NSPS = 'ticket-events';
+
+  constructor(public platform: Platform, public authService: AuthService) {
+    const url = `${this.HOST}:${this.PORT}/${this.NSPS}`;
+    const options = {
+      query: {
+        'uid': this.authService.getUid(),
+      }
+    };
+    super({ url, options });
+
+
     console.log('IS PLATFORM RUNNING ANRDOIRD '), platform.is('android');
     console.log('HERE');
+  }
+
+  connect() {
+    super.connect();
   }
 }
