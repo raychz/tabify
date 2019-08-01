c5a31fb2f215aa76dd2aa9fce8cf7cce07471531
Raymond Chavez <chavezr@mit.edu>
2019-07-31 21:21:48 -0400

    When clicking Pay Tab, redirect users to payment method page if no method is on file.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { user, global, community } from './example-stories';
 import { ILocation } from '../../interfaces/location.interface';
+import { LoaderService } from '../../services/utilities/loader.service';
+import { PaymentService } from '../../services/payment/payment.service';
+import { AlertService } from '../../services/utilities/alert.service';
 
 export interface Story {
   location: ILocation;
@@ -24,18 +27,57 @@ export class HomePage {
     global,
   };
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public loader: LoaderService, public paymentService: PaymentService, public alert: AlertService) { }
 
   segmentChanged(event: any) {
     console.log(event);
   }
 
-  payNewTab() {
-    this.navCtrl.push(
-      'PayPage',
-      {},
-      { animate: true, animation: 'md-transition', direction: 'forward' }
-    );
+  async payNewTab() {
+    await this.loader.present();
+    try {
+      const paymentMethods = await this.paymentService.getPaymentMethods();
+
+      // If user has a payment method on file, proceed to pay workflow
+      // Otherwise, take user to payment method entry page
+      if (paymentMethods && paymentMethods.length > 0) {
+        await this.loader.dismiss();
+        this.navCtrl.push(
+          'PayPage',
+          {},
+          { animate: true, animation: 'md-transition', direction: 'forward' }
+        );
+      } else {
+        await this.loader.dismiss();
+        const alert = this.alert.create({
+          title: 'No Payment Method',
+          message: `To get started with paying your tab, enter a payment method on the next screen.`,
+          buttons: [
+            {
+              text: 'Ok',
+            },
+          ],
+        });
+        await alert.present();
+        this.navCtrl.push(
+          'PaymentMethodsPage',
+          {},
+          { animate: true, animation: 'md-transition', direction: 'forward' }
+        );
+      }
+    } catch {
+      await this.loader.dismiss();
+      const alert = this.alert.create({
+        title: 'Error',
+        message: `Whoops, something went wrong. Please try again.`,
+        buttons: [
+          {
+            text: 'Ok',
+          },
+        ],
+      });
+      await alert.present();
+    }
   }
 
   refresh(refresher: any) {
