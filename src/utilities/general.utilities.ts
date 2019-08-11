8d1bd5d83150260c0028b52fa8fa41f113bc6bd7
Raymond Chavez <chavezr@mit.edu>
2019-08-11 15:16:13 -0600

    Add comments to ticket and general utitlity functions.

--- src/utilities/general.utilities.ts
+++ src/utilities/general.utilities.ts
@@ -1,7 +1,15 @@
 // Stateless helper functions
 
+/**
+ * Convenience function that sets a timeout for `t` milliseconds.
+ * @param t 
+ */
 export const sleep = async (t: number) => new Promise(r => setTimeout(r, t));
 
+/**
+ * Abbreviates a user's name. Ex: `abbreviateName('Raymond Chavez')` => `'Raymond C.'`
+ * @param name 
+ */
 export const abbreviateName = (name: string) => {
   const nameSplit: string[] = name.split(' ');
   const firstName = nameSplit.shift();
@@ -11,6 +19,7 @@ export const abbreviateName = (name: string) => {
 
 /**
  * Returns an 's' if count !== 1
+ * @param count
  */
 export const plurality = (count: number) => {
   return count !== 1 ? 's' : '';
