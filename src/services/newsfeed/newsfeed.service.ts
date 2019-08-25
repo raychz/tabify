7e3b573b89e9cfa03ce4b43775214a139072a172
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-08-25 13:44:43 -0400

    check if logged in user liked a story, empty heart

--- src/services/newsfeed/newsfeed.service.ts
+++ src/services/newsfeed/newsfeed.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { StoryService } from '../story/story.service';
+import { AuthService } from '../auth/auth.service';
+import { _localeFactory } from '@angular/core/src/application_module';
 
 // This class holds that data structure that is used to represent content on the newsfeed
 @Injectable()
@@ -9,15 +11,35 @@ export class NewsfeedService {
     // Inside each ticket, there is a story object
     tickets: any;
 
-    constructor(private storyService: StoryService) { }
+    constructor(
+        private storyService: StoryService,
+        private authService: AuthService,
+    ) { }
 
     async initializeNewsfeed() {
         await this.getUserTicketsFromServer();
+        await this.determineStoriesLikedByUser();
     }
 
     async getUserTicketsFromServer() {
         let userTickets = await this.storyService.getUserStories();
         this.tickets = userTickets.tickets;
+        return this.tickets;
+    }
+
+    async determineStoriesLikedByUser() {
+        const loggedInUserId = this.authService.getUid();
+
+        for (let i = 0; i < this.tickets.length; i++) {
+            for (let j = 0; j < this.tickets[i].story.likes.length; j++) {
+                if (this.tickets[i].story.likes[j].user.uid === loggedInUserId) {
+                    this.tickets[i].story.likedByLoggedInUser = true;
+                    break;
+                }
+                
+            }
+        }
+        console.log(this.tickets);
         return this.tickets;
     }
 
@@ -31,10 +53,13 @@ export class NewsfeedService {
 
     incrementLikeCount(ticketId: number, storyId: number) {
         this.tickets[this.findIndexOfTicket(ticketId, storyId)].story.like_count += 1;
+        this.tickets[this.findIndexOfTicket(ticketId, storyId)].story.likedByLoggedInUser = true;
     }
 
     decrementLikeCount(ticketId: number, storyId: number) {
         this.tickets[this.findIndexOfTicket(ticketId, storyId)].story.like_count -= 1;
+        this.tickets[this.findIndexOfTicket(ticketId, storyId)].story.likedByLoggedInUser = false;
+
     }
 
     loadingLike(ticketId: number, storyId: number, loading: boolean) {
