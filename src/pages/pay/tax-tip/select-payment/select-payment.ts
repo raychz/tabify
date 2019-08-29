9210fc5afaca1b3678ccb554a3548f3ea3b25572
Raymond Chavez <chavezr@mit.edu>
2019-08-29 01:44:33 -0400

    Add sleep for more pleasant UX after selecting payment method.

--- src/pages/pay/tax-tip/select-payment/select-payment.ts
+++ src/pages/pay/tax-tip/select-payment/select-payment.ts
@@ -30,6 +30,8 @@ export class SelectPaymentPage {
   async selectPaymentMethod(method: any) {
     // Pop back to Checkout page and disable the other radio options
     this.canSelect = false;
+    // Sleep for more pleasant UX after selecting payment method
+    await sleep(300);
     await this.navCtrl.pop();
   }
 }
