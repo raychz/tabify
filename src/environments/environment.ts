7368f211f1b9d68d1dba59c91cdd5d3f2de98bae
Raymond Chavez <chavezr@mit.edu>
2020-04-10 21:54:58 -0600

    Move envs and ignore zone related error stack.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,22 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// Local development environment
 export const environment = {
-  production: false
+	version: '0.0.0',
+	production: false,
+	serverUrl: 'http://localhost:3000',
+	firebaseConfig: {
+		apiKey: 'AIzaSyCyOe9TB0DTkDtX8B_3ddutEfN0nOy-WdU',
+		authDomain: 'tabify-40746.firebaseapp.com',
+		databaseURL: 'https://tabify-40746.firebaseio.com',
+		projectId: 'tabify-40746',
+		storageBucket: 'tabify-40746.appspot.com',
+		messagingSenderId: '1044249255712',
+	},
+	spreedlyEnvKey: 'Iu3UapkcfklJXqLJV61vbJsp1dl',
+	ablyKey: 'N0WaAg.cYTfTg:XJnXR07hwbcc6Sxz',
+	sentryDsn: 'https://8b1f236b4d1f4053a24eefb1f8aea326@sentry.io/2364364',
 };
 
 /*
@@ -13,4 +27,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
