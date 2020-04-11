b8a00c6527e96a5e78984f59f6037b110b0dd9e8
Raymond Chavez <chavezr@mit.edu>
2020-04-11 16:08:16 -0600

    Update app module with deps.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,10 +10,53 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+////***********************
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+// import { Ionic2RatingModule } from 'ionic2-rating';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '@tabify/env';
+import { AuthService } from '../services/auth/auth.service';
+// import { SharedPayModule } from '../pages/pay/shared-pay.module';
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: environment.sentryDsn,
+  release: `tabify-frontend@${environment.version}`,
+  environment: environment.production ? 'production' : 'development',
+  enabled: environment.production
+});
+
+@Injectable({ providedIn: 'root' })
+export class TabifyErrorHandler implements ErrorHandler {
+  constructor(public auth: AuthService) { }
+
+  handleError(error) {
+    console.log('Sending error report to Sentry.', error);
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
+////***********************
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot({
+      // TODO: Make scrollPadding false?
+      // scrollPadding: false
+    }), 
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    // TODO: Update to use a modern ionic rating module
+    // Ionic2RatingModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
