82ce6a508dbf87e9c06b817608e3053fcc266014
= <sahil@tabifyapp.com>
2020-05-23 16:04:08 -0400

    changed pay workflow routes so that they are not children of PayPage, should be a better lazy loading?

--- src/app/pages/home/pay/pay-routing.module.ts
+++ src/app/pages/home/pay/pay-routing.module.ts
@@ -6,20 +6,19 @@ const routes: Routes = [
     {
       path: '',
       component: PayPage,
-      children: [
-        {
-          path: 'select-items',
-          loadChildren: () => import('./select-items/select-items.module').then( m => m.SelectItemsPageModule)
-        },
-        {
-          path: 'tax-tip',
-          loadChildren: () => import('./tax-tip/tax-tip.module').then( m => m.TaxTipPageModule)
-        },
-        {
-          path: 'status',
-          loadChildren: () => import('./status/status.module').then( m => m.StatusPageModule)
-        },
-      ],
+
+    },
+    {
+      path: 'select-items',
+      loadChildren: () => import('./select-items/select-items.module').then( m => m.SelectItemsPageModule)
+    },
+    {
+      path: 'tax-tip',
+      loadChildren: () => import('./tax-tip/tax-tip.module').then( m => m.TaxTipPageModule)
+    },
+    {
+      path: 'status',
+      loadChildren: () => import('./status/status.module').then( m => m.StatusPageModule)
     },
 ];
 
