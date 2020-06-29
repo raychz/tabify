fb2ed6d44ab9d9e51f5b9bfd2d194814d49deb64
= <sahil@tabifyapp.com>
2020-06-28 21:35:08 -0400

    split pane

--- src/app/hamburger-menu/hamburger-menu.page.ts
+++ src/app/hamburger-menu/hamburger-menu.page.ts
@@ -10,47 +10,58 @@ import { NavController } from '@ionic/angular';
 })
 export class HamburgerMenuPage implements OnInit {
   selectedPath = '';
-  pages: {title: string, icon: string, click: () => void}[];
+  pages = [
+    {
+      url: 'home',
+      title: 'Home',
+      icon: 'home-outline',
+    },
+    {
+      url: 'payment-methods',
+      title: 'Payment Methods',
+      icon: 'card-outline',
+    },
+    {
+      url: 'auth',
+      title: 'Log Out', // will be log in if guest authenticated
+      icon: 'log-out-outline',
+    },
+    {
+      url: 'help',
+      title: 'Help',
+      icon: 'help-outline',
+    },
+    {
+      url: 'settings',
+      title: 'Settings',
+      icon: 'settings-outline',
+    },
+  ];
 
   constructor(
     public router: Router,
     public auth: AuthService,
     public navCtrl: NavController,
     ) {
-    // this.router.events.subscribe((event: RouterEvent) => {
-    //   if (event && event.url) {
-    //     this.selectedPath = event.url;
-    //   }
-    // });
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event && event.url) {
+        this.selectedPath = event.url;
+        // remove first / of path
+        if (this.selectedPath.startsWith('/')) {
+          this.selectedPath = this.selectedPath.substring(1);
+        }
+      }
+    });
   }
 
   ngOnInit() {
   }
 
-  public viewPaymentMethods() {
-    console.log(this.navCtrl);
-    this.navCtrl.navigateRoot('payment-methods');
-  }
-
-  public viewHelp() {
-    this.navCtrl.navigateRoot('help');
-  }
-
-  public viewSettings() {
-    this.navCtrl.navigateRoot('settings');
-  }
-
-  public viewHome() {
-    this.navCtrl.navigateRoot('home');
-  }
-
-  public viewAuth() {
-    this.navCtrl.navigateRoot('auth');
-  }
-
-  public logout() {
-    this.auth.signOut();
-    this.viewAuth();
+  public openPage(page: any) {
+    if (page.title === 'Log Out') {
+      this.auth.signOut();
+    }
+    this.navCtrl.navigateRoot(page.url);
   }
 
 }
