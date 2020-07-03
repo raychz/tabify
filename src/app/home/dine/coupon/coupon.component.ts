66eb40333ab51c79b6fbe62008bd81978c48014d
= <sahil@tabifyapp.com>
2020-07-03 16:59:01 -0400

    coupons and dine

--- src/app/home/dine/coupon/coupon.component.ts
+++ src/app/home/dine/coupon/coupon.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CouponService } from 'src/services/coupon/coupon.service';
+import { CouponGroup, CouponOffOf, CouponType } from 'src/enums';
+import { Coupon } from 'src/interfaces/coupon.interface';
+import { NavController, AlertController } from '@ionic/angular';
+import { LoaderService } from 'src/services/utilities/loader.service';
 
 @Component({
   selector: 'app-coupon',
@@ -7,8 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CouponComponent implements OnInit {
 
-  constructor() { }
+  expandedCouponId: number;
+  // Expose enum to template
+  CouponOffOf = CouponOffOf;
+  // Expose enum to template
+  CouponType = CouponType;
 
-  ngOnInit() {}
+  constructor(
+    public navCtrl: NavController,
+    public couponService: CouponService,
+    public alertCtrl: AlertController,
+    public loader: LoaderService,
+  ) {}
 
+  async ngOnInit() {
+      await this.getCoupons();
+  }
+
+  expandCoupon(coupon: Coupon) {
+    if (this.expandedCouponId === coupon.id) {
+      this.expandedCouponId = -1;
+    } else {
+      this.expandedCouponId = coupon.id;
+    }
+  }
+
+  async getCoupons() {
+    const loading = await this.loader.create();
+    loading.present();
+    try {
+      await this.couponService.getCoupons();
+      this.expandedCouponId = -1;
+    } catch(e) {
+      // Sentry.captureException(e);
+      const alert = await this.alertCtrl.create({
+        header: 'Network Error',
+        message: `Please check your connection and try again.`,
+      });
+      alert.present();
+    }
+    await loading.dismiss();
+  }
+
+  // async redeemCoupon(coupon: Coupon) {
+  //   this.couponService.selectCoupon(coupon);
+  //   if (!this.fullCouponsPage) {
+  //     await this.navCtrl.pop()
+  //   } else {
+  //     await this.navCtrl.navigateForward('TabLookupPage', coupon.location);
+  //   }
+  // }
+
+  // cancel() {
+  //   this.navCtrl.pop({
+  //     animate: true,
+  //     animation: 'md-transition',
+  //     direction: 'back',
+  //   });
+  // }
 }
--- src/app/home/dine/coupon/coupon.module.ts
+++ src/app/home/dine/coupon/coupon.module.ts
@@ -2,13 +2,11 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CouponComponent } from './coupon.component';
-import { LocationModule } from '../locations/component/location.component.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    LocationModule
   ],
   declarations: [CouponComponent],
   exports: [CouponComponent],
